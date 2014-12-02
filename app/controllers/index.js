d5df75fd0a40385d7c6696dae7157adfa002e25e
bttf <adnanchowdhury88@gmail.com>
2014-12-02 11:57:15 -0500

    added fb login button; getting ready to do oauth

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    loginFb: function() {
+      window.location.href = 'https://www.facebook.com/dialog/oauth?client_id=996369437044844&redirect_uri=http://chewbonga.com'; 
+    },
+
     save: function() {
       //console.log('body: %s', this.get('body'));
       //console.log('title: %s', this.get('title'));
