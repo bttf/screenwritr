269c10ccb4475d4d30a52ab7cb388b400ce34e16
bttf <adnanchowdhury88@gmail.com>
2014-12-02 22:27:39 -0500

    simple-auth and torii basic implementation working

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,22 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     loginFb: function() {
-      window.location.href = 'https://www.facebook.com/dialog/oauth?client_id=996369437044844&redirect_uri=http://chewbonga.com'; 
+      //window.location.href = 'https://www.facebook.com/dialog/oauth?client_id=996369437044844&redirect_uri=http://chewbonga.com'; 
+      var _this = this;
+      this.get('session').authenticate('simple-auth-authenticator:torii', 'facebook-oauth2').then(function(provider, data) {
+        console.log('authentication successful');
+        console.log(_this.get('session.authorizationCode'));
+        console.dir(_this.get('session'));
+      });
+    },
+
+    doShit: function() {
+      console.log('here be ye session');
+      console.dir(this.get('session'));
+      console.dir(this.get('session.content'));
+      console.dir(this.get('session.isAuthenticated'));
+      console.dir(this.get('session.store'));
+      console.log('hey: ' + this.get('session.authorizationCode'));
     },
 
     save: function() {
