ef0a188dd122bc8c80a2f291f5fc061c965a9f4b
bttf <adnanchowdhury88@gmail.com>
2014-12-08 10:49:52 -0500

    tidied up code; added login menu

--- app/models/entry.js
+++ app/models/entry.js
@@ -4,6 +4,8 @@ var attr = DS.attr;
 
 export default DS.Model.extend({
   title: attr('string'),
-  body: attr('string')
+  body: attr('string'),
+  created: attr('date'),
+  modified: attr('date')
 });
 
