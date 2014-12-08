fee247c5587043e037baae591b2c8fcbf8bb7992
bttf <adnanchowdhury88@gmail.com>
2014-12-08 11:52:10 -0500

    login prompt checks for valid fields

--- app/controllers/c-p.js
+++ app/controllers/c-p.js
@@ -1,5 +1,31 @@
 import Ember from 'ember';
+import ENV from 'screenwritr/config/environment';
 
 export default Ember.ObjectController.extend({
-  needs: 'index'
+  //needs: 'index',
+  loginError: false,
+  actions: {
+    login: function() {
+      console.log('debug, c-p controller, login action');
+      var ref = new window.Firebase('https://' + ENV.APP.firebaseInstance + '.firebaseio.com')
+
+      if (validateFields()) {
+        if (this.get('loginError')) this.set('loginError', false);
+
+      }
+      else {
+        console.log('debug, c-p controller, login action, loginError, Fields not valid');
+        this.set('loginError', 'Invalid credentials');
+      }
+
+      function validateFields() {
+        var email = $('#email').val();
+        var pass = $('#password').val();
+        if (!email || email === '' ||
+            !pass || pass === '')
+          return false;
+        return true;
+      }
+    }
+  }
 });
