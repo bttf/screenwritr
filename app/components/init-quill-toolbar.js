10e2486f230d8b15cb3d37fb4d9f76036eeb2d59
bttf <adnanchowdhury88@gmail.com>
2014-12-11 12:42:59 -0500

    align buttons working

--- app/components/init-quill-toolbar.js
+++ app/components/init-quill-toolbar.js
@@ -3,71 +3,48 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   initQuillToolbar: function() {
     window.quillEditor.addModule('toolbar', { container: '#toolbar' });
-    //var anchors;
-    //if (anchors = this.fetchAnchors(this.fetchToolbar())) {
-      //for (var i = 0; i < anchors.length; i++) {
-        //var a = anchors[i];
-        //a.addEventListener('mouseup', (function(e, _this) {
-          //return function() {
-            //$(e).addClass('active');
-            //_this.disableOtherActives(e);
-            //_this.enableStyle(e.innerHTML);
-          //}
-        //})(a, this));
-      //}
-    //}
+
+    var anchors = this.fetchAnchors(this.fetchToolbar('align-toolbar'));
+
+    if (anchors) {
+      for(var i = 0; i < anchors.length; i++) {
+        var a = anchors[i];
+        console.log('adding events for anchor:', a.className);
+        a.addEventListener('mouseup', (function(a, _this) {
+          return function() {
+            $(a).addClass('ql-active');
+            _this.disableOtherActives(anchors, a);
+            _this.enableStyle(a.childNodes[0].className);
+          };
+        })(a, this));
+      }
+    }
   }.on('didInsertElement'),
 
-  disableOtherActives: function(e) {
-    var a = this.fetchAnchors(this.fetchToolbar());
-    for (var i = 0; i < a.length; i++) {
-      if (e !== a[i]) {
-        $(a[i]).removeClass('active');
+  disableOtherActives: function(list, item) {
+    for (var i = 0; i < list.length; i++) {
+      if (item !== list[i]) {
+        $(list[i]).removeClass('ql-active');
       }
     }
   },
 
-  enableStyle: function(style) {
-    switch(style) {
-      case "Title":
-        console.log('title');
-        window.quillEditor.focus();
-        window.quillEditor.prepareFormat('bold', true);
-        window.quillEditor.prepareFormat('align', 'center');
-        window.quillEditor.focus();
-        break;
-      case "Credits":
-        console.log('credits');
-        window.quillEditor.focus();
-        window.quillEditor.prepareFormat('bold', true);
-        window.quillEditor.prepareFormat('bold', false);
-        window.quillEditor.prepareFormat('align', 'center');
-        window.quillEditor.focus();
-        break;
-      case "Slugline":
-        console.log('slugline');
-        window.quillEditor.focus();
-        window.quillEditor.prepareFormat('bold', true);
-        window.quillEditor.prepareFormat('bold', false);
-        window.quillEditor.prepareFormat('align', 'left');
-        window.quillEditor.focus();
-        break;
-      case "Action":
-        console.log('action');
-        window.quillEditor.focus();
-        window.quillEditor.prepareFormat('bold', true);
-        window.quillEditor.prepareFormat('bold', false);
-        window.quillEditor.prepareFormat('align', 'left');
-        window.quillEditor.focus();
-        break;
-      case "Dialogue":
-        console.log('dialogue');
-        window.quillEditor.focus();
-        window.quillEditor.prepareFormat('bold', true);
-        window.quillEditor.prepareFormat('bold', false);
-        window.quillEditor.prepareFormat('align', 'center');
-        window.quillEditor.focus();
-        break;
+  enableStyle: function(className) {
+    console.log('debug, enableStyle');
+    console.log('debug, enableStyle, className', className);
+    if (className.indexOf('align-left') > -1) {
+      window.quillEditor.focus();
+      window.quillEditor.prepareFormat('align', 'left');
+      window.quillEditor.focus();
+    } else if (className.indexOf('align-center') > -1) {
+      window.quillEditor.focus();
+      window.quillEditor.prepareFormat('align', 'center');
+      window.quillEditor.focus();
+    } else if (className.indexOf('align-right') > -1) {
+      console.log('debug, enableStyle right');
+      window.quillEditor.focus();
+      window.quillEditor.prepareFormat('align', 'right');
+      window.quillEditor.focus();
     }
   },
 
@@ -79,8 +56,8 @@ export default Ember.Component.extend({
     });
   },
 
-  fetchToolbar: function() {
-    return document.getElementById('toolbar') || false;
+  fetchToolbar: function(id) {
+    return document.getElementById(id) || false;
   },
 
   editor: window.quillEditor
