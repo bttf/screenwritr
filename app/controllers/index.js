9fcc23894bda2c9fb1d000bcb2d65419ca4e7458
bttf <adnanchowdhury88@gmail.com>
2014-12-16 16:30:45 -0500

    removed c-p context; added afterModel logic to display user entries

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import ENV from 'screenwritr/config/environment';
 
 export default Ember.ObjectController.extend({
+  loginError: false,
   saveTimeout: false,
   listTimeouts: [],
 
@@ -16,20 +18,20 @@ export default Ember.ObjectController.extend({
 
   saveEntry: function(_this) {
     var body = JSON.stringify(window.quillEditor.getContents()),
-      title = this.generateTitle(body);
-    
-    _this.set('body', body);
-    _this.set('title', title);
+    title = this.generateTitle(body);
+
+    _this.set('model.body', body);
+    _this.set('model.title', title);
 
     if (Ember.isEmpty(_this.get('created'))) {
-      _this.set('created', new Date());
-      _this.set('modified', new Date());
+      _this.set('model.created', new Date());
+      _this.set('model.modified', new Date());
     } else {
-      _this.set('modified', new Date());
+      _this.set('model.modified', new Date());
     }
 
     _this.get('store').find('user', _this.get('session.uid')).then(function(user) {
-      _this.set('user', user);
+      _this.set('model.user', user);
 
       _this.get('model').save().then(function(entry) {
         user.get('entries').pushObject(entry);
@@ -66,6 +68,67 @@ export default Ember.ObjectController.extend({
       this.get('listTimeouts').push(this.get('timeoutId'));
 
       return;
+    }, 
+
+    login: function() {
+      var email = $('#email').val();
+      var pass = $('#password').val();
+
+      if (!Ember.isEmpty(email) && !Ember.isEmpty(pass)) {
+        // toggle off error state if exists
+        if (this.get('loginError')) this.set('loginError', false);
+
+        // if user exists, log in, otherwise create user 
+        var _this = this;
+        userExists(this, email).then(function(user) {
+          _this.get('session').authenticate('authenticator:firebase', {
+            user: user,
+            password: pass
+          }).then(function() {
+            console.log('authentication successful');
+          }, function(err) {
+            _this.set('loginError', err);
+          });
+        }, 
+
+        function() {
+          var newUser = _this.get('store').createRecord('user', {
+            email: email,
+            password: pass
+          });
+          _this.transitionToRoute('createUser', newUser);
+        });
+      } 
+
+      else {
+        this.set('loginError', 'Please fill in all fields');
+      }
+    },
+
+    logout: function() {
+      var _this = this;
+      this.get('session').invalidate('authenticator:firebase').then(function() {
+        console.log('authentication invalidated');
+        _this.transitionToRoute('index');
+      }, function(err) {
+        console.log('I\'m afraid I can\'t do that');
+      });
+
     }
   }
 });
+
+function userExists(_this, email) {
+  return new Ember.RSVP.Promise(function(resolve, reject) {
+    _this.get('store').find('user').then(function(users) {
+      var user = users.filter(function(user) {
+        return user.get('email').toLowerCase() === email.toLowerCase();
+      });
+      if (user.length > 0) {
+        resolve(user.objectAt(0));
+      } else {
+        reject('No users found');
+      }
+    });
+  });
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.get('store').createRecord('entry', {
-      body: 'Begin writing here ...'
+      body: 'Enter text here ...'
     });
+  },
+
+  afterModel: function(model) {
+    if (this.get('session.isAuthenticated')) {
+      this.get('store').find('user', this.get('session.uid')).then(function(user) {
+        Ember.set(model, 'entries', user.get('entries'));
+      });
+    }
   }
 });
