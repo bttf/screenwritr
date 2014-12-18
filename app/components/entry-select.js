b42a5029c8a9516baddbefe539cc476c07340fa9
bttf <adnanchowdhury88@gmail.com>
2014-12-18 13:58:45 -0500

    dropdown cog menu

--- app/components/entry-select.js
+++ app/components/entry-select.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['entry-select'],
+  classNames: ['row', 'entry-select'],
   classNameBindings: ['selected'],
 
   click: function() {
