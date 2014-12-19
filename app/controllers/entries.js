6dcb47a95d5d69da4827633327bc914021061089
bttf <adnanchowdhury88@gmail.com>
2014-12-19 00:44:03 -0500

    modal dialog for entry delete

--- app/controllers/entries.js
+++ app/controllers/entries.js
@@ -1,13 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  needs: 'index',
   sortProperties: ['modified'],
   sortAscending: false,
 
   actions: {
     deleteEntry: function(entry) {
-      console.log('we be deletin out in de entries controlla');
+      // delete entry from user.entries 
+      this.get('store').find('user', this.get('session.uid')).then(function(user) {
+        user.get('entries').removeObject(entry);
+        user.save();
+      });
+    
       entry.deleteRecord();
+      entry.save().then(function() {
+      }, function(err) {
+        // TODO propogate error to UI
+        console.log('error deleting', err);
+      });
     }
   }
 });
