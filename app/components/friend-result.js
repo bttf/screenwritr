116b8ef40bfd01330397478f23f78d422fcacb53
bttf <adnanchowdhury88@gmail.com>
2014-12-24 13:06:24 -0500

    friend-select on hover styling

--- app/components/friend-result.js
+++ app/components/friend-result.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['row', 'friend-result']
+  classNames: ['row', 'friend-result'],
+  classNameBindings: ['selected'],
+  selected: false,
+  mouseEnter: function() {
+    this.set('selected', true);
+  },
+  mouseLeave: function() {
+    this.set('selected', false);
+  }
 });
