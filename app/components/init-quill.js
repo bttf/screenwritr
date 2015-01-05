8e01e0251af730a326b2b822dfc2225f1887a94b
bttf <adnanchowdhury88@gmail.com>
2015-01-05 13:16:47 -0500

    height set to innerWidth; on resize too

--- app/components/init-quill.js
+++ app/components/init-quill.js
@@ -2,7 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   initQuill: function() {
+    var sizeRatio = 0.66;
+
+    Ember.$('#editor').height(sizeRatio * window.innerHeight);
+
+    $(window).resize(function() {
+      Ember.$('#editor').height(sizeRatio * window.innerHeight);
+    });
+
     window.quillEditor = new window.Quill('#editor');
     window.quillEditor.focus();
+
   }.on('didInsertElement')
 });
