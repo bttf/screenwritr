19274bfcdd281cf43880c3fd954aa95e973a6ab1
bttf <adnanchowdhury88@gmail.com>
2015-01-28 22:02:09 -0500

    new login page

--- app/router.js
+++ app/router.js
@@ -1,14 +1,22 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
-  this.route('users');
-  this.route('createUser', { path: '/createUser/:email' });
-  this.route('user', { path: '/user/:user_id' });
+  this.route("users");
+
+  this.route("createUser", {
+    path: "/createUser/:email"
+  });
+
+  this.route("user", {
+    path: "/user/:user_id"
+  });
+
+  this.route("login");
 });
 
 export default Router;
