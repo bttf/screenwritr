20793f399a74848b1f7fc87a6ed41fb0531f96fb
bttf <adnanchowdhury88@gmail.com>
2015-01-30 04:03:14 -0500

    help panel

---
+++ app/components/help-panel.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['help-panel'],
+  autoSize: function() {
+    Ember.$('.help-panel').css('height', window.innerHeight);
+  }.on('didInsertElement')
+});
