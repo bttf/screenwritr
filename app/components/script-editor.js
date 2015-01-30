a0338483ee4f273433bb873bbd30d3922cfe67f3
bttf <adnanchowdhury88@gmail.com>
2015-01-30 17:47:06 -0500

    script model; edit/preview pane

--- app/components/script-editor.js
+++ app/components/script-editor.js
@@ -3,18 +3,30 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['script-editor'],
   editMode: true,
+  initialize: function() {
+    var sizeRatio = 0.66;
+
+    Ember.$('.editor').height(sizeRatio * window.innerHeight);
+
+    Ember.$(window).resize(function() {
+      Ember.$('.editor').height(sizeRatio * window.innerHeight);
+    });
+
+    // unsure about quill editor atm
+    //
+    // window.quillEditor = new window.Quill('.editor');
+    // window.quillEditor.focus();
+  }.on('didInsertElement'),
+
   actions: {
     toggleEditMode: function() {
       this.set('editMode', !this.get('editMode'));
     }
-  }
+  },
 
-  // keyUp: function() {
-  //   if (this.get('isAuthenticated')) {
-  //     console.log('debug, auto-saving');
-  //     this.sendAction('autoSaveAction', true);
-  //   }
-  // },
+  keyUp: function() {
+    // this.set('script.body', JSON.stringify(window.quillEditor.getContents()));
+  },
   // 
   // actions: {
   //   manualSave: function() {
--- app/models/script.js
+++ app/models/script.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
+var attr = DS.attr;
+
 export default DS.Model.extend({
-  
+  body: attr('string')
 });
