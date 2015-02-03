530644596f83125b233b38696568fd66cfdba80f
bttf <adnanchowdhury88@gmail.com>
2015-02-03 10:20:41 -0500

    fountain helper handling undefined/null

--- app/helpers/fountain-to-html.js
+++ app/helpers/fountain-to-html.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
 export function fountainToHtml(input) {
+  if (Ember.isEmpty(input)) {
+    return input;
+  }
+
   var output = window.fountain.parse(input);
   var htmlOutput = "<div class='title-page'>" +
                   output.html.title_page +
                   "</div><div class='script-pages'>" +
                   output.html.script +
                   "</div>";
+
   return new Ember.Handlebars.SafeString(htmlOutput);
 }
 
