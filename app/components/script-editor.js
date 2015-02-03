3e435debfe6f43a02db4577f0490a7b978e79c71
bttf <adnanchowdhury88@gmail.com>
2015-02-03 09:35:47 -0500

    textarea capture tabs to indent

--- app/components/script-editor.js
+++ app/components/script-editor.js
@@ -5,11 +5,23 @@ var $ = Ember.$;
 export default Ember.Component.extend({
   classNames: ['script-editor'],
   editMode: true,
+
   initialize: function() {
+    // auto-size editor to innerHeight
     var sizeRatio = 0.75;
     $('.editor').height(sizeRatio * window.innerHeight);
     $(window).resize(function() {
       $('.editor').height(sizeRatio * window.innerHeight);
+    });
+
+    // capture tabs to indent
+    $('#scriptInput').keydown(function(e) {
+      if (e.keyCode == 9) {
+        e.preventDefault();
+        var s = this.selectionStart;
+        this.value = this.value.substring(0, s) + '\t' + this.value.substring(this.selectionEnd);
+        this.selectionEnd = s + 1;
+      }
     });
   }.on('didInsertElement'),
 
