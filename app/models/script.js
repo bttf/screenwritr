e00c22f92a1b9b2c3761b39da30167490ddc1d5b
bttf <adnanchowdhury88@gmail.com>
2015-02-03 13:40:59 -0500

    new script default value

--- app/models/script.js
+++ app/models/script.js
@@ -3,5 +3,11 @@ import DS from 'ember-data';
 var attr = DS.attr;
 
 export default DS.Model.extend({
+  title: attr('string', { 
+    defaultValue: (function() {
+      var date = window.moment(new Date()).format('HH:mm - MM/DD/YYYY');
+      return 'Untitled @ ' + date;
+    })()
+  }),
   body: attr('string')
 });
