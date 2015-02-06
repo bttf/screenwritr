8ca36f283e11371722faf32ebbe2d97e5700e4a6
bttf <adnanchowdhury88@gmail.com>
2015-02-06 11:24:33 -0500

    open UI decent

--- app/controllers/open.js
+++ app/controllers/open.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  selectedScript: '',
+
+  actions: {
+    selectScript: function(script) {
+      this.set('selectedScript', script);
+    }
+  }
 });
