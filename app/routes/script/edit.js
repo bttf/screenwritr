f22ceba2736b9a3860190640280854acdb9dd93c
bttf <adnanchowdhury88@gmail.com>
2015-02-06 12:13:57 -0500

    script/edit controller

--- app/routes/script/edit.js
+++ app/routes/script/edit.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('script', params.id);
+  },
+
+  setupController: function(controller, model) {
+    controller.set('model', model);
+    controller.set('controllers.application.script', model);
   }
 });
