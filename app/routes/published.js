1cb303532f354132b7a383aa664cd4e0318a3a41
bttf <adnanchowdhury88@gmail.com>
2015-05-24 15:45:09 -0400

    removed extraneous features; published route changes

--- app/routes/published.js
+++ app/routes/published.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('script', params.id);
+  },
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    if (Ember.isEmpty(model.get('body'))) {
+      controller.set('isScriptEmpty', true);
+    }
   }
 });
--- app/routes/script/new.js
+++ app/routes/script/new.js
@@ -16,7 +16,7 @@ export default Ember.Route.extend({
       } else {
         return _this.store.createRecord('script', {
           uid: user
-        });
+        }).save();
       }
     });
   },
